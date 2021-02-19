188cd6ec138bb031ac845cf40779c385daadebaa
Christian Pereira <christianpbsd@gmail.com>
2021-02-19 17:29:43 -0300

    function

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+/* Função
+function somar(x, y){
+    let total = x + y;
+
+    return total;
+}
+
+let resultado = somar(25, 100);
+
+console.log(resultado);
+*/
+
+/*
+    function alterar(titulo){
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("campo").value = titulo;
+
+}
+
+alterar("Título de exemplo");
+*/
+
+/*  Função
+
+    function alterar(){
+    document.getElementById("titulo").innerHTML = "Título alterado";
+    document.getElementById("campo").value = "Campo alterado";
+
+}
+
+alterar();
+*/
+
 /*
  let campo = document.getElementById("campo")
 campo.value = "Nome do usuário";
