e325bbc2caf0d3eed1db27d55ed1b24e8f6d7c96
Christian Pereira <christianpbsd@gmail.com>
2021-02-19 17:10:28 -0300

    JS

---
+++ script.js
@@ -0,0 +1,27 @@
+
+/*
+ let campo = document.getElementById("campo")
+campo.value = "Nome do usuário";
+*/
+
+/* 
+let titulo = document.getElementById('titulo');
+
+titulo.innerHTML = "Segundo Título";
+*/
+
+/*  
+document.getElementById("titulo").innerHTML = "Olá Mundo!";
+*/
+
+/* básico de JS
+let nome = "teste";
+let idade = 100;
+let salvo = true;
+
+let tipo = typeof idade;
+
+console.log(tipo);
+*/
+
+
