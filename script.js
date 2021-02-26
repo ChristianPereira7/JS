2a724b1f1b06d2541893494ac92cab32d6b599cf
Christian Pereira <christianpbsd@gmail.com>
2021-02-26 16:29:36 -0300

    Styles

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+function azul(){
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
+}
+
+function vermelho(){
+    limpar();
+    document.getElementById("titulo").classList.add('vermelho');
+}
+
+
+function verde(){
+    limpar();
+    document.getElementById("titulo").classList.add('verde');
+}
+
+function limpar(){
+    document.getElementById("titulo").classList.remove('azul');
+    document.getElementById("titulo").classList.remove('vermelho');
+    document.getElementById("titulo").classList.remove('verde');
+}
+
+
 
 /* Evento de Teclado
 function digitou(e){
